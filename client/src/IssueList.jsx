51c7eab6fbaac5bcdf3f0166068ef54e88c87a1d
Shongsu <chinhouu1992@gmail.com>
2019-07-29 21:38:05 -0400

    client

---
+++ client/src/IssueList.jsx
@@ -0,0 +1,65 @@
+import IssueFilter from './IssueFilter.jsx';
+import IssueTable from './IssueTable.jsx';
+import IssueAdd from './IssueAdd.jsx';
+
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
+function jsonDateReviver(key, value) {
+  if (dateRegex.test(value)) return new Date(value);
+  return value;
+}
+
+export default class IssueList extends React.Component {
+    constructor() {
+      super();
+      this.state = { issues: [] };
+      this.createIssue = this.createIssue.bind(this);
+    }
+  
+    componentDidMount() {
+      this.loadData();
+    }
+  
+    async loadData() {
+      try {
+        const response = await fetch('/api/issues');
+        const body = await response.text();
+        const result = JSON.parse(body, jsonDateReviver);
+        this.setState({ issues: result });
+      } catch (err) {
+        console.log(err);
+      };
+    }
+  
+    async createIssue(newIssue) {
+      try {
+        const response = await fetch('/api/issues', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(newIssue),
+        });
+        const body = await response.text();
+        const result = JSON.parse(body, jsonDateReviver);
+        if (response.ok) {
+          const newIssues = this.state.issues.concat(result);
+          this.setState({ issues: newIssues });
+        } else {        
+          alert("Failed to add issue: " + result.message)
+        }
+      } catch (e) {
+        alert(`Error in sending data to server: ${e.message}`);
+      }
+    }
+  
+    render() {
+      return (
+        <React.Fragment>
+          <h1>Issue Tracker</h1>
+          <IssueFilter />
+          <hr />
+          <IssueTable issues={this.state.issues} />
+          <hr />
+          <IssueAdd createIssue={this.createIssue} />
+        </React.Fragment>
+      );
+    }
+  }
