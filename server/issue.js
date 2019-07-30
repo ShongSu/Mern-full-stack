04a752f8cdbe01e31a305a598946af478f1aed3f
Shongsu <chinhouu1992@gmail.com>
2019-07-29 20:06:01 -0400

    backend

---
+++ server/issue.js
@@ -0,0 +1,40 @@
+const { getDb, getNextSequence } = require('./db.js');
+
+async function issueList() {
+    const db = getDb();
+    const issues = await db.collection('issues').find({}).toArray();
+    return issues;
+}
+
+function validateIssueV2(issue) {
+    const errors = [];
+    if (issue.title.length < 3) {
+        errors.push('Field "title" must be at least 3 characters long.')
+    }
+    if (issue.status == 'Assigned' && !issue.owner) {
+        errors.push('Field "owner" is required when status is "Assigned"');
+    }
+    if (errors.length > 0) {
+        return errors;
+    }
+}
+
+async function issueAdd(issue) {
+    const db = getDb();
+    issue.created = new Date();
+    if (!issue.status)
+        issue.status = 'New';
+    const err = validateIssueV2(issue)
+    if (err) {
+        return { message: `Invalid requrest: ${err}` };
+    }
+    issue.id = await getNextSequence('issues');
+
+    const result = await db.collection('issues').insertOne(issue);
+    const savedIssue = await db.collection('issues')
+        .findOne({ _id: result.insertedId });
+
+    return savedIssue;
+}
+
+module.exports = { issueList, issueAdd };
--- server/server.js
+++ server/server.js
@@ -1,65 +1,24 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
+const { connectToDb } = require('./db.js');
+const issue = require('./issue.js');
 
 app.use(bodyParser.json());
-
-
-const url = 'mongodb://localhost/issuetracker';
-let db;
-
-async function getNextSequence(name) {
-  const result = await db.collection('counters').findOneAndUpdate(
-    { _id: name },
-    { $inc: { current: 1 } },
-    { returnOriginal: false },
-  );
-  return result.value.current;
-}
-
-async function issueAdd(issue) {
-  issue.created = new Date();
-  if (!issue.status)
-    issue.status = 'New';
-  const err = validateIssueV2(issue)
-  if (err) {
-    return { message: `Invalid requrest: ${err}` };
-  }
-  issue.id = await getNextSequence('issues');
-
-  const result = await db.collection('issues').insertOne(issue);
-  const savedIssue = await db.collection('issues')
-    .findOne({ _id: result.insertedId });
-
-  return savedIssue;
-}
-
-async function issueList() {
-  const issues = await db.collection('issues').find({}).toArray();
-  return issues;
-}
-
-async function connectToDb() {
-  const client = new MongoClient(url, { useNewUrlParser: true });
-  await client.connect();
-  console.log('Connected to MongoDB at', url);
-  db = client.db();
-}
 
 app.get('/hello', (req, res) => {
   res.send('Hello World');
 });
 
 app.get('/api/issues', (req, res) => {
-  issueList().then(issues => {
+  issue.issueList().then(issues => {
     res.send(issues);
   })
 });
 
 app.post('/api/issues', (req, res) => {
   const newIssue = req.body;
-  issueAdd(newIssue).then((result) => {    
+  issue.issueAdd(newIssue).then((result) => {    
     if(result.message) {
       res.status(422).json(result);
       return;
@@ -68,18 +27,7 @@ app.post('/api/issues', (req, res) => {
   });
 });
 
-function validateIssueV2(issue) {
-  const errors = [];
-  if (issue.title.length < 3) {
-    errors.push('Field "title" must be at least 3 characters long.')
-  }
-  if (issue.status == 'Assigned' && !issue.owner) {
-    errors.push('Field "owner" is required when status is "Assigned"');
-  }
-  if (errors.length > 0) {
-    return errors;
-  }
-}
+
 
 (async function () {
   try {
