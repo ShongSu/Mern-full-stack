2aeafd592c30bc9857f19a9d81925e5240703e0f
Shongsu <chinhouu1992@gmail.com>
2019-07-29 22:36:16 -0400

    hot module replacement

--- client/uiserver.js
+++ client/uiserver.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 const proxy = require('http-proxy-middleware');
+const enableHMR = true;
+
+if (enableHMR) {
+  console.log('Adding dev middleware, enabling HMR');
+  const webpack = require('webpack');
+  const devMiddleware = require('webpack-dev-middleware');
+  const hotMiddleware = require('webpack-hot-middleware');
+  const config = require('./webpack.config.js');
+  config.entry.app.push('webpack-hot-middleware/client');
+  config.plugins = config.plugins || [];
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  const compiler = webpack(config);
+  app.use(devMiddleware(compiler));
+  app.use(hotMiddleware(compiler));
+}
 
 app.use(express.static('public'));
 app.use('/api', proxy({ target: 'http://localhost:3000' }));
